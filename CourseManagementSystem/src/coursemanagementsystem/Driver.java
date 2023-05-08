346ab35ff535155f2dd089e70eba4a39d4367bf9
andyhou05 <131478635+andyhou05@users.noreply.github.com>
2023-05-08 14:03:01 -0400

    Update Driver.java

--- CourseManagementSystem/src/coursemanagementsystem/Driver.java
+++ CourseManagementSystem/src/coursemanagementsystem/Driver.java
@@ -3,9 +3,20 @@ package coursemanagementsystem;
 public class Driver {
 
     public static void main(String[] args) {
-        Student s1 = new Student(1, "password1", "fName", "lName");
-        Student s2 = new Student(2, "password2", "fName", "lName");
-        Student s3 = new Student(3, "password3", "fName", "lName");
+        Student s1 = new Student("Student","1",1, "password1");
+        Student s2 = new Student("Student","2",2, "password2");
+        Student s3 = new Student("Student","3",3, "password3");
+        Student s4 = new Student("Student","4",4, "password4");
+        Instructor i1 = new Instructor("Teacher", "1", 4, "pass");
+        Admin adm = new Admin(9999, "pw");
+        Course calculus = new Course(102, "Calculus I", i1);
+        
+        //adding calculus to s1 and s2 Courses list
+        s1.getCourses().add(calculus);
+        s2.getCourses().add(calculus);
+        //adding grades to check class average
+        calculus.getGrades().put(s1, 60);
+        calculus.getGrades().put(s2, 80);
 
         boolean exitCondition = false;
 
@@ -13,8 +24,13 @@ public class Driver {
             switch (UserInputManager.mainMenu()) {
                 case 1:
                     Account account = UserInputManager.login();
-                    UserInputManager.displayActions(account);
-                    break;
+                    if (account == null) {
+                        break;
+                    } else {
+                        System.out.println("\nWelcome " + account.getFirstName()+" "+account.getLastName());
+                        UserInputManager.displayActions(account);
+                        break;
+                    }
 
                 case 2:
                     System.out.println("See you again!");
